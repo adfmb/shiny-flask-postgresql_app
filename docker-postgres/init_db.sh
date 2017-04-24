5897bc4de91ff8b9f5670086bff5a39b7c4e4cf2
Eduardo Martinez <eduardo.martinez@integritas.mx>
2017-04-24 17:04:13 -0500

    ultimos toques

--- docker-postgres/init_db.sh
+++ docker-postgres/init_db.sh
@@ -3,6 +3,9 @@
 # this script is runned when the docker container is built
 # it imports the base database structure and create the database for the tests
 
+# Tomado de aqui: http://stackoverflow.com/questions/29600369/starting-and-populating-a-postgres-container-in-docker
+# con algunos cambios
+
 DATABASE_NAME="dpa"
 DB_DUMP_LOCATION="/tmp/psql_data/iris.sql"
 
@@ -14,8 +17,6 @@ psql --username postgres <<EOSQL
   CREATE DATABASE "$DATABASE_NAME";
   GRANT ALL PRIVILEGES ON DATABASE "$DATABASE_NAME" TO postgres;
 EOSQL
-
-echo "1"
 
 # clean sql_dump - because I want to have a one-line command
 
@@ -36,8 +37,6 @@ sed 's/^ *//' -i "$DB_DUMP_LOCATION"
 
 # append new line at the end (suggested by @Nicola Ferraro)
 sed -e '$a\' -i "$DB_DUMP_LOCATION"
-
-echo "2"
 
 # import sql_dump
 psql --username postgres "$DATABASE_NAME" < "$DB_DUMP_LOCATION";
